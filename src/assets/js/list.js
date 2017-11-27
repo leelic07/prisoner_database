b63cb7735c08d6c108622dab231fd4197c71319b
AbrahamJay <abrahamjay@163.com>
2017-11-27 15:06:41 +0800

    侧边栏功能完善

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -16,6 +16,43 @@ export const ListData = [
                     },
                     {
                         thrName: '其他处理'
+                    },
+                    {
+                        thrName: '1111111'
+                    }
+                ]
+            },
+            {
+                sedName: '监外执行'
+            },
+            {
+                sedName: '出监管理'
+            },
+            {
+                sedName: '4444444'
+            }
+        ]
+    },
+    {
+        ListName: '办公助理',
+        secondaryMenu: [
+            {
+                sedName: '收监管理'
+            },
+            {
+                sedName: '减刑假释',
+                thirdMenu: [
+                    {
+                        thrName: '刑罚变动'
+                    },
+                    {
+                        thrName: '监区办理'
+                    },
+                    {
+                        thrName: '其他处理'
+                    },
+                    {
+                        thrName: '2222222'
                     }
                 ]
             },
@@ -28,9 +65,34 @@ export const ListData = [
         ]
     },
     {
-        ListName: '办公助理'
-    },
-    {
-        ListName: '档案管理'
+        ListName: '档案管理',
+        secondaryMenu: [
+            {
+                sedName: '收监管理'
+            },
+            {
+                sedName: '减刑假释',
+                thirdMenu: [
+                    {
+                        thrName: '刑罚变动'
+                    },
+                    {
+                        thrName: '监区办理'
+                    },
+                    {
+                        thrName: '其他处理'
+                    },
+                    {
+                        thrName: '3333333'
+                    }
+                ]
+            },
+            {
+                sedName: '监外执行'
+            },
+            {
+                sedName: '出监管理'
+            }
+        ]
     }
 ]
