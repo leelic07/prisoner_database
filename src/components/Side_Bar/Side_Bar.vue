29fdf1143dea226af1e605813cb36dac5cc00c9d
AbrahamJay <abrahamjay@163.com>
2017-11-28 14:36:48 +0800

    侧边栏隐藏

--- src/components/Side_Bar/Side_Bar.vue
+++ src/components/Side_Bar/Side_Bar.vue
@@ -1,54 +1,84 @@
 <template>
-  <div id="side-bar" class="col-xs-4 pull-left" :style="{ width : isShow?'':'50px'}">
-    <div class="side-top">
-        <div class="side-avatar pull-left"></div>
-        <div class="side-right" :style="{ display : isShow?'':'none'}">
-            <p class="side-content">管理员</p>
-            <p class="glyphicon glyphicon-map-marker pull-left"></p>
-            <p class="side-right-bottom">长沙监狱</p>    
+    <div>
+        <div id="side-bar" class="col-xs-4 pull-left" :style="{ width : isShow?'':'0'}">
+            <div class="side-top row">
+                <div class="side-avatar pull-left"></div>
+                <div class="side-right" :style="{ display : isShow?'':'none'}">
+                    <p class="side-content col-xs-24">管理员</p>
+                    <p class="glyphicon glyphicon-map-marker col-xs-1 pull-left"></p>
+                    <p class="side-right-bottom col-xs-10">长沙监狱</p>    
+                </div>
+            </div>
+            <ul class="col-xs-24">
+                <!--一级菜单-->
+                <template v-for="(item,index) in ListData">
+                    <li class="list col-xs-24" @click="toggle(index)" :class="nowIndex==index?'active':''" :key="index">
+                        <div class="col-xs-4 list-icon">
+                            <span class="li-icon"></span>
+                        </div>
+                        <div class="col-xs-8 col-xs-offset-1" :style="{ display : isShow?'':'none'}">
+                            <span>{{item.ListName}}</span>  
+                        </div>
+                        <div class="col-xs-2 pull-right">   
+                            <span :class="nowIndex==index?'arrow-bottom':'arrow-right'" :style="{ display : isShow?'':'none'}"></span>
+                        </div>
+                    </li>
+                    <!--二级菜单-->
+                    <template v-if="item.secondaryMenu">
+                        <ul v-show="nowIndex==index" :key="index" class="col-xs-24">
+                            <li class="list-two" v-for="(tmp,ind) in item.secondaryMenu" :key="ind">
+                                <div @click="toggleTwo(ind)">
+                                    <div class="col-xs-14 col-xs-offset-6">
+                                        <span>{{tmp.sedName}}</span>
+                                    </div>
+                                    <div v-if="tmp.thirdMenu" class="col-xs-2 pull-right">   
+                                        <span :class="nowInd==ind?'arrow-bottom':'arrow-right'"></span>
+                                    </div>
+                                </div>
+                                
+                                <!--三级菜单-->
+                                <template v-if="tmp.thirdMenu">
+                                    <ul v-show="nowInd==ind" class="col-xs-24">
+                                        <li class="list-three on row col-xs-24" v-for="(rmp,inde) in tmp.thirdMenu" :key="inde">
+                                            <div class="col-xs-14 col-xs-offset-8">
+                                                <span>{{rmp.thrName}}</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </template>
+                            </li>                             
+                        </ul>
+                    </template>
+                </template>
+            </ul>
+        </div>
+
+        <div v-show="!isShow" id="side-collapse" class="col-xs-4 pull-left" :style="{ width : isShow?'':'3.7%'}">
+            <div class="side-top row">
+                <div class="side-avatar pull-left"></div>
+            </div>
+            <ul class="col-xs-24">
+                <!--一级菜单-->
+                    <li class="list col-xs-24">
+                        <div class="list-icon">
+                            <span class="li-icon"></span>
+                        </div>
+                    </li>
+                    <ul class="list-col-ul">
+                        <li class="list-third">
+                            <span>123</span>    
+                        </li>
+                        <li class="list-third">
+                            <span>123</span>  
+                        </li>
+                        <li class="list-third">
+                            <span>123</span>  
+                        </li>
+                    </ul>
+            </ul>
         </div>
     </div>
-    <ul class="col-xs-24">
-        <!--一级菜单-->
-        <template v-for="(item,index) in ListData">
-            <li class="list col-xs-24" @click="toggle(index)" :class="nowIndex==index?'active':''" :key="index">
-                <div class="col-xs-4 list-icon">
-                    <span class="li-icon"></span>
-                </div>
-                <div class="col-xs-8 col-xs-offset-1" :style="{ display : isShow?'':'none'}">
-                    <span>{{item.ListName}}</span>  
-                </div>
-                <div class="col-xs-2 pull-right" :style="{ display : isShow?'':'none'}">   
-                    <span :class="nowIndex==index?'arrow-bottom':'arrow-right'"></span>
-                </div>
-            </li>
-            <!--二级菜单-->
-            <template v-if="item.secondaryMenu">
-                <ul v-show="nowIndex==index" :key="index" class="col-xs-24">
-                    <li class="list-two" v-for="(tmp,ind) in item.secondaryMenu" :key="ind" @click="toggleTwo(ind)">
-                        <div class="col-xs-14 col-xs-offset-6">
-                            <span>{{tmp.sedName}}</span>
-                        </div>
-                        <div v-if="tmp.thirdMenu" class="col-xs-2 pull-right">   
-                            <span :class="nowInd==ind?'arrow-bottom':'arrow-right'"></span>
-                        </div>
-                        <!--三级菜单-->
-                        <template v-if="tmp.thirdMenu">
-                            <ul v-show="nowInd==ind" class="col-xs-24">
-                                <li class="list-three on row col-xs-24" v-for="(rmp,inde) in tmp.thirdMenu" :key="inde">
-                                    <div class="col-xs-14 col-xs-offset-8">
-                                        <span>{{rmp.thrName}}</span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </template>
-                    </li>                             
-                </ul>
-            </template>
-        </template>
-    </ul>
-  </div>
-</template>
+</template>  
 
 <script>
     import { ListData } from './../../assets/js/list'
@@ -66,17 +96,19 @@
             //一级菜单点击缩放方法
             toggle(index){
                 if(this.nowIndex==index){
-                    this.nowIndex=-1
+                    this.nowIndex = -1
+                    // this.nowInd = -1
                 }else{
                     this.nowIndex=index
+                    this.nowInd = -1
                 }     
             },
             //二级菜单点击缩放方法
             toggleTwo(ind){
                 if(this.nowInd==ind){
-                    this.nowInd=-1
+                    this.nowInd = -1
                 }else{
-                    this.nowInd=ind
+                    this.nowInd = ind
                 }     
             }
         },
@@ -114,10 +146,11 @@
         top 46px
         bottom 0px
         color white
-        overflow-y:auto;
-        transition: all .2s linear;
+        overflow-y auto
+        transition all .2s linear
         .side-top
-            padding 20px 16px 20px 7px
+            padding 20px 16px 14px 9px
+            width 260px
             position relative
             .side-avatar
                 width 38px
@@ -130,23 +163,24 @@
                 .side-content
                     margin-bottom 10px
                 .side-right-bottom
-                    margin-left 17px
+                    margin-left 9px
                     margin-bottom 0
         .list
             height 44px
             line-height 44px
             font-size 14px
             .list-icon
-                margin-top 1px  
-                &:hover
-                    background-color #29b0a3
-                .li-icon
-                    display:inline-block
-                    width 15px
-                    height 15px
-                    background url(../../assets/img/settings.png) 0 0 no-repeat
-                    margin-left 16px
-                    margin-right 14px
+                margin-top 1px
+                margin-left 1px  
+            &:hover
+                background-color #29b0a3
+            .li-icon
+                display:inline-block
+                width 15px
+                height 15px
+                background url(../../assets/img/settings.png) 0 0 no-repeat
+                margin-left 16px
+                margin-right 14px
         .list-two
             height 38px
             line-height 38px
@@ -161,4 +195,49 @@
             background #182125
             &:hover
                 background-color #13191c
+    #side-collapse
+        background bgcolor
+        position absolute
+        top 46px
+        bottom 0px
+        color white
+        transition all .2s linear
+        .side-top
+            padding 20px 16px 14px 9px
+            width 260px
+            position relative
+            .side-avatar
+                width 38px
+                height 40px
+                background:url(../../assets/img/user.png) 0 0 no-repeat
+                background-size 38px 40px
+        .list
+            height 44px
+            line-height 44px
+            font-size 14px
+            width 100%
+            .list-icon
+                margin-top 1px
+                margin-left 1px  
+            &:hover
+                background-color #222D32
+            .li-icon
+                display:inline-block
+                width 15px
+                height 15px
+                background url(../../assets/img/settings.png) 0 0 no-repeat
+                margin-left 16px
+                margin-right 14px
+        .list-col-ul
+            width  241px
+            color #000
+            margin-left 100%
+            .list-third
+                height 40px 
+                background #29B0A3
+                line-height 40px 
+                font-size 14px
+                color #fff
+                
+
 </style>
